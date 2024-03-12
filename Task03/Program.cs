4ff4e752d1f4070ed451739a3b1224d07db14115
Lyokakz <lenusik__rybka@mail.ru>
2024-03-12 21:06:51 +0500

    Add a solution HomeWorkC#:Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -6,3 +6,16 @@
 [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
 */
 
+void PrintArrayReversed(int[] array, int i)
+{
+    if (i >= 0)
+    {
+        Console.Write($"{array[i]} ");
+        PrintArrayReversed(array, i - 1);
+    }
+}
+
+int[] array = { 1, 2, 5, 0, 10, 34 };
+Console.Write($"[{String.Join(", ", array)}] => ");
+PrintArrayReversed(array, array.Length - 1);
+

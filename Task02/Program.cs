bea3de7b62098e406b7cb48d49d7faff4f0e5e46
Lyokakz <lenusik__rybka@mail.ru>
2024-03-12 21:00:53 +0500

    Add a solution HomeWorkC#:Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -3,7 +3,41 @@
 Аккермана с помощью рекурсии. Даны два
 неотрицательных числа m и n. 
 
-m = 2, n = 3 -> A(m,n) = 29
+n = 3, m = 2 -> A(n,m) = 29
 */
 
+int AckermanFunction(int n, int m)
+{
+    if (n < 0 || m < 0)
+    {
+        // Возвращаем -1 или другое значение, указывающее на ошибку
+        Console.WriteLine("Ошибка! Функция Аккермана работает только для положительных чисел.");
+        return -1;
+    }
+
+    while (n != 0)
+    {
+        if (m == 0)
+        {
+            m = 1;
+        }
+        else
+        {
+            m = AckermanFunction(n, m - 1);
+        }
+
+        n--;
+    }
+
+    return m + 1;
+}
+
+Console.Write("Введите значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int result = AckermanFunction(n, m);
+Console.WriteLine($"n={n}, m={m} -> A(n,m) = {result}");
 
